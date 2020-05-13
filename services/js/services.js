a405ebfbe3220a9336164dd501b8611d0b53f46a
Айзек Венс <ajzekv@gmail.com>
2020-05-13 22:08:46 +0200

    fix(services) fix item hiding method

--- services/js/services.js
+++ services/js/services.js
@@ -1,14 +1,31 @@
+let servicesItems = document.querySelectorAll('.services__item--not-ms, .services__item--hidden');
+for (let item of servicesItems) {
+    item.children[0].tabIndex = "-1";
+}
+
 let buttonMore = document.querySelector('.button-more');
 const buttonMoreDefaultText = buttonMore.textContent;
 
-let servicesList = document.querySelector('.services__list');
+let servicesHiddenItems = document.querySelectorAll('.services__item--hidden');
 
 buttonMore.addEventListener('click', function() {
     if (buttonMore.classList.toggle('button-more--active')) {
-        servicesList.classList.add('services__list--opened');
         buttonMore.textContent = buttonMore.dataset.another_text;
+        attrToggle(false);
     } else {
-        servicesList.classList.remove('services__list--opened');
         buttonMore.textContent = buttonMoreDefaultText;
+        attrToggle(true);
+    }
+
+    function attrToggle(add) {
+        for (let item of servicesHiddenItems) {
+            if (add) {
+                item.classList.add('services__item--hidden');
+                item.children[0].tabIndex = -1;
+            } else {
+                item.classList.remove('services__item--hidden');
+                item.children[0].tabIndex = 0;
+            }
+        }
     }
 });
