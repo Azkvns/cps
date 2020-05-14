bb64a15b77b9b8094750dd0ad92aaecc0c980676
Айзек Венс <ajzekv@gmail.com>
2020-05-14 10:27:11 +0200

    fix:(services) fix adaptivity

--- services/js/services.js
+++ services/js/services.js
@@ -1,31 +1,31 @@
-let servicesItems = document.querySelectorAll('.services__item--not-ms, .services__item--hidden');
-for (let item of servicesItems) {
-    item.children[0].tabIndex = "-1";
-}
+function toggleItemsStyle(arr, changeClass) {
+    for (let item of arr) {
+        if (window.getComputedStyle(item).position === 'absolute') {
+            item.children[0].tabIndex = -1;
+            if (changeClass) item.classList.add('services__item--visible');
+        } else {
+            item.children[0].tabIndex = 0;
+            if (changeClass) item.classList.remove('services__item--visible');
+        }
+    }
+};
+
+let servicesItems = document.querySelector('.services__list').children;
+toggleItemsStyle(servicesItems, false);
+
+window.matchMedia('(min-width: 768px) and (max-width: 1119px),').addEventListener('change', function(evt) {
+    toggleItemsStyle(servicesItems, false);
+});
 
 let buttonMore = document.querySelector('.button-more');
 const buttonMoreDefaultText = buttonMore.textContent;
 
-let servicesHiddenItems = document.querySelectorAll('.services__item--hidden');
-
 buttonMore.addEventListener('click', function() {
     if (buttonMore.classList.toggle('button-more--active')) {
         buttonMore.textContent = buttonMore.dataset.another_text;
-        attrToggle(false);
+        toggleItemsStyle(servicesItems, true);
     } else {
         buttonMore.textContent = buttonMoreDefaultText;
-        attrToggle(true);
-    }
-
-    function attrToggle(add) {
-        for (let item of servicesHiddenItems) {
-            if (add) {
-                item.classList.add('services__item--hidden');
-                item.children[0].tabIndex = -1;
-            } else {
-                item.classList.remove('services__item--hidden');
-                item.children[0].tabIndex = 0;
-            }
-        }
+        toggleItemsStyle(servicesItems, true);
     }
 });
